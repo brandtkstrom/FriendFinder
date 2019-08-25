1e6fc00142cd376e11869ca450d0c78ad10cf31f
Brandt Strom <brandtkstrom@gmail.com>
2019-08-25 01:14:28 -0700

    implement survey result modal

---
+++ app/public/survey.js
@@ -0,0 +1,63 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('button#submit').addEventListener('click', evt => {
+        evt.preventDefault();
+
+        // Validate name and photo entered
+        const name = document.querySelector('#input-name').value.trim();
+        const photo = document.querySelector('#input-photo').value.trim();
+
+        if (name === '') {
+            alert('You must enter your name.');
+            return;
+        } else if (photo === '') {
+            alert('You must provide a photo link.');
+            return;
+        }
+
+        // Validate all questions answered
+        const selectElems = document.querySelectorAll('select.custom-select');
+        const options = Array.from(selectElems);
+        const missing = options.find(
+            option => !option.value || option.value === ''
+        );
+        if (missing) {
+            alert('You must answer all questions.');
+            return;
+        }
+
+        // Submit new Friend info
+        const scores = options.map(option => parseInt(option.value));
+        const postData = {
+            name: name,
+            photo: photo,
+            scores: scores
+        };
+
+        fetch('/api/friends', {
+            method: 'POST',
+            body: JSON.stringify(postData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(async res => {
+                let match = '';
+                // No match found...
+                if (res.status === 404) {
+                    match = 'No compatible match found.';
+                } else {
+                    const data = await res.json();
+                    match = data.name;
+                }
+
+                // Display modal with match
+                document.querySelector('#survey').reset();
+                $('#friend-match').text(match);
+                $('#friend-modal').modal();
+            })
+            .catch(err => {
+                console.log(`ERROR: ${err}`);
+                alert('An error occurred. Please try again.');
+            });
+    });
+});
